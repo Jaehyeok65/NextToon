c970e3b92a2e5938303203a98547ebf42d7d854e
Lee, Jaehyeok <dlwogur15@gmail.com>
2024-03-01 21:51:59 +0900

    feat : 메인 홈페이지 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
-import { QueryClient } from "@tanstack/react-query";
-import { QueryClientProvider } from "@tanstack/react-query";
+'use client';
+
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import style from '@/style/main.module.css';
 
 export default function Home() {
-  return (
-    <>
-    안녕하세요
-    </>
-  );
+
+  const router = useRouter();
+
+  
+    return (
+        <div className={style.background}>
+            <div onClick={() => router.push('/list')}>전체웹툰 바로가기</div>
+            <div onClick={() => router.push('/list/naver')}>네이버 웹툰 바로가기</div>
+            <div onClick={() => router.push('/list/kakao')}>카카오 웹툰 바로가기</div>
+            <div onClick={() => router.push('/list/kakaopage')}>카카오페이지 웹툰 바로가기</div>
+            <div onClick={() => router.push('/bookmark')}>북마크 바로가기</div>
+        </div>
+    );
 }
