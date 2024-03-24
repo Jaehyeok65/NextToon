aa0d39e75eb5ba356f6cbd385df61cac924995a1
Lee, Jaehyeok <dlwogur15@gmail.com>
2024-03-24 21:58:34 +0900

    feat : 캐러셀 버튼 위치 조정

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -3,16 +3,17 @@
 import React, { useEffect, useState } from 'react';
 import styles from '@/style/carousel.module.css';
 import Card from './Card';
-import { FaArrowRight } from "react-icons/fa";
-import { FaArrowLeft } from "react-icons/fa";
+import { FaArrowRight } from 'react-icons/fa';
+import { FaArrowLeft } from 'react-icons/fa';
 
 const Carousel = ({ list }: { list: any[] }) => {
     const [current, setCurrent] = useState(5); //현재 페이지
-    const [windowWidth, setWindowWidth] = useState(typeof window !== 'undefined' ? window.innerWidth : 0);
+    const [windowWidth, setWindowWidth] = useState(
+        typeof window !== 'undefined' ? window.innerWidth : 0
+    );
     const [depth, setDepth] = useState(1); //default값을 1
     const end = list.length - 1;
     const lastindex = end - depth + 1;
-
 
     const handleResize = () => {
         setWindowWidth(window.innerWidth);
@@ -28,19 +29,18 @@ const Carousel = ({ list }: { list: any[] }) => {
     }, []); // 빈 배열을 전달하여 컴포넌트가 처음 마운트될 때만 이펙트가 실행되도록 합니다.
 
     useEffect(() => {
-        if(windowWidth < 600) {
+        if (windowWidth < 600) {
             setDepth(1);
-        }
-        else if(windowWidth < 1000) {
+        } else if (windowWidth < 1000) {
             setDepth(2);
-        }
-        else {
+        } else {
             setDepth(3);
         }
 
-         // 5초마다 getNextPage 함수 호출
-         const intervalId = setInterval(() => {
-            getNextPage(current);
+        // 5초마다 getNextPage 함수 호출
+        const intervalId = setInterval(() => {
+            const nextPage = getNextPage(current);
+            +setCurrent(nextPage);
         }, 5000);
 
         // 컴포넌트가 언마운트되면 clearInterval 호출하여 타이머 제거
@@ -50,25 +50,28 @@ const Carousel = ({ list }: { list: any[] }) => {
     }, [windowWidth, current]);
 
     const getNextPage = (page: number) => {
-        if (page + 1 > lastindex) {
-            setCurrent(0);
-        } else {
-            setCurrent(page + 1);
-        }
+        return page + 1 > lastindex ? 0 : page + 1;
     };
 
     const getPrevPage = (page: number) => {
-        if (page - 1 < 0) {
-            setCurrent(lastindex);
-        } else {
-            setCurrent(page - 1);
-        }
+        return page - 1 < 0 ? lastindex : page - 1;
     };
 
+    const handleNextClick = () => {
+        const nextPage = getNextPage(current);
+        setCurrent(nextPage);
+    };
+
+    const handlePrevClick = () => {
+        const prevPage = getPrevPage(current);
+        setCurrent(prevPage);
+    };
 
     return (
         <div className={styles.carousel}>
-            <button className={styles.button} onClick={() => getPrevPage(current)}><FaArrowLeft color='blue' size='20px'/></button>
+            <button className={styles.button} onClick={handlePrevClick}>
+                <FaArrowLeft />
+            </button>
             <div className={styles.innercarousel}>
                 {list
                     .slice(current, current + depth)
@@ -83,7 +86,9 @@ const Carousel = ({ list }: { list: any[] }) => {
                         />
                     ))}
             </div>
-            <button className={styles.button2} onClick={() => getNextPage(current)}><FaArrowRight color='blue' size='20px'/></button>
+            <button className={styles.button2} onClick={handleNextClick}>
+                <FaArrowRight />
+            </button>
         </div>
     );
 };
