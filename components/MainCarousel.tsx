322a3baea818de3aad0d198363b6e4b700f93368
Lee, Jaehyeok <dlwogur15@gmail.com>
2024-04-12 18:21:07 +0900

    style : 바로가기 위치 수정

--- components/MainCarousel.tsx
+++ components/MainCarousel.tsx
@@ -1,24 +1,30 @@
-"use client";
-
+'use client';
 
 import React from 'react';
 import Link from 'next/link';
-import Carousel from "./Carousel"
-import styles from '@/style/carousel.module.css'
+import Carousel from './Carousel';
+import styles from '@/style/carousel.module.css';
 
-
-const MainCarousel = ({ list, title, address } : {list : any, title : string, address : string}) => {
-
+const MainCarousel = ({
+    list,
+    title,
+    address,
+}: {
+    list: any;
+    title: string;
+    address: string;
+}) => {
     return (
         <div className={styles.maincarousel}>
             <div className={styles.maininner}>
-            <h4>{title}</h4>
-            <Link href={address} prefetch>바로가기</Link>
+                <h4>{title}</h4>
+                <Link href={address} prefetch>
+                    <h4>바로가기</h4>
+                </Link>
             </div>
             <Carousel list={list} />
         </div>
-    )
-}
-
+    );
+};
 
 export default MainCarousel;
